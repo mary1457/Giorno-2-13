2e709dcd7817eb111c79b7727c1e1eee52e5a69e
mary1457 <piabaldoin@gmail.com>
2024-09-25 09:51:10 +0200

    modifiche

--- src/esercizio3/Main3.java
+++ src/esercizio3/Main3.java
@@ -1,18 +1,15 @@
 package esercizio3;
 import java.util.Scanner;
+
 public class Main3 {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-
-
         while (true) {
-
             System.out.print("Inserisci una stringa (digita ':q' per uscire): ");
             String input = scanner.nextLine();
-
 
             if (input.equals(":q")) {
                 System.out.println("Esci");
@@ -20,21 +17,13 @@ public class Main3 {
             }
 
 
-            StringBuilder risultato = new StringBuilder();
-            for (int i = 0; i < input.length(); i++) {
-                risultato.append(input.charAt(i));
-                if (i < input.length() - 1) {
-                    risultato.append(", ");
-                }
-            }
+            String[] caratteri = input.split("");
+            String risultato = String.join(", ", caratteri);
 
-
-            System.out.println("Caratteri separati da virgola: " + risultato);
+            System.out.println(risultato);
         }
-
 
         scanner.close();
     }
-
-
 }
+
